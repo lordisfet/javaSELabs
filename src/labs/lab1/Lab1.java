bf3a203f9b487643468b6c85817f17ba14d5abe8
lordisfet <143896699+lordisfet@users.noreply.github.com>
2025-03-16 06:11:45 +0200

    Add comments to Lab1.java

--- src/labs/lab1/Lab1.java
+++ src/labs/lab1/Lab1.java
@@ -8,18 +8,21 @@ public class Lab1 {
     public static void main(String[] args) {
         Employee emp;
 
+        // This block of code don't throw exception
         try {
             emp = new Employee("Max", "Savchenko", 999999.99);
         } catch (EmployeeException e) {
             System.out.println("Exception: " + e.getMessage());
         }
 
+        // This block of code throw exception because SuperPuperLongName has length more then MAX_NAME_LENGTH
         try {
             emp = new Employee("SuperPuperLongName", "NormalSurname", 123456);
         } catch (EmployeeException e) {
             System.out.println("Exception: " + e.getMessage());
         }
 
+        // This block of code throw exception because SuperPuperLongSurname has length more then MAX_NAME_LENGTH
         try {
             emp = new Employee("NormalName", "SuperPuperLongSurname", 123456);
         } catch (EmployeeException e) {
